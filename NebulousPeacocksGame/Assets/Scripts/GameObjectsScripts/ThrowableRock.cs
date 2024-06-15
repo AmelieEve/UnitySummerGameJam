3c00ef1f01ca61e9d6b79c9729ac791db1614a3b
Léo-Paul HUAR <leopaul.huar@gmail.com>
2024-06-15 16:15:44 +0200

    Add throwable rock
    Signed-off-by: Léo-Paul HUAR <leopaul.huar@gmail.com>

--- NebulousPeacocksGame/Assets/Scripts/GameObjectsScripts/ThrowableRock.cs
+++ NebulousPeacocksGame/Assets/Scripts/GameObjectsScripts/ThrowableRock.cs
@@ -1,18 +1,77 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ThrowableRock : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Rigidbody rb;
+    private bool isHeld = false;
+    public Vector3 heldOffset = new Vector3(0, 0, 0);
+    private bool canPickUp = false;
+    public float throwForce = 10f;
+    public GameObject player = null;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        if (player == null)
+        {
+            Debug.LogError("Player is not set in the inspector!");
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (isHeld)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                Throw();
+            }
+        } else if (canPickUp)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                PickUp();
+            }
+        }
+    }
+
+    public void PickUp()
+    {
+        isHeld = true;
+        rb.isKinematic = true;
+        transform.SetParent(player.transform);
+        transform.localPosition = heldOffset;
+    }
+
+    void Throw()
+    {
+        isHeld = false;
+        transform.SetParent(null);
+        rb.isKinematic = false;
+        rb.AddForce(player.transform.forward * throwForce, ForceMode.Impulse);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Breakable"))
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            canPickUp = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            canPickUp = false;
+        }
     }
 }
