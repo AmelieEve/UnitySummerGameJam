4d9d9402afaf0060e0aa9d3378935bceb74c005d
Amélie E <amelie.eveno@insa-rennes.fr>
2024-06-18 21:05:30 +0200

    add skyboxes and new ancient level

--- NebulousPeacocksGame/Assets/Scripts/GameObjectsScripts/RitualManager.cs
+++ NebulousPeacocksGame/Assets/Scripts/GameObjectsScripts/RitualManager.cs
@@ -2,12 +2,15 @@ using UnityEngine;
 
 public class RitualManager : MonoBehaviour
 {
+    public bool requireGlyph;
+    public bool requireSacredObject;
+
     [HideInInspector]
     public bool presentedToGlyph;
     public ThrowableRock sacredObject;
 
     public bool IsRitualCompleted()
     {
-        return (presentedToGlyph && sacredObject.GetIsHeld());
+        return ((requireGlyph && presentedToGlyph) || (requireSacredObject && sacredObject.GetIsHeld()));
     }
 }
